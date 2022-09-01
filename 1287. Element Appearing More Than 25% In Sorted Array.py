7b581ef32588ec552b8e3a9a668f79399ba42f17
Don Xavier <98073418+DonXavierdev@users.noreply.github.com>
2022-09-01 21:45:10 +0530

    python

---
+++ 1287. Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,18 @@
+'''
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+ 
+'''
+for i in set(arr):
+    if arr.count(i)/len(arr)*100>25:
+        return i
