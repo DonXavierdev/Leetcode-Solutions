a8c57e197cae1801917e1e122cf25e6d2ad2005a
Don Xavier <98073418+DonXavierdev@users.noreply.github.com>
2023-05-15 22:43:36 +0530

    Q.1528 solution

---
+++ 1528. Shuffle String.py
@@ -0,0 +1,29 @@
+'''
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+ 
+
+Example 1:
+
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+'''
+def restoreString(s,indices):
+    out=[0]*len(s)
+    res=''
+    for i in range(len(s)):
+        out[indices[i]]=s[i]
+    for i in out:
+        res+=i
+    return res
+print(restoreString(s = "codeleet", indices = [4,5,6,7,0,2,1,3]))
